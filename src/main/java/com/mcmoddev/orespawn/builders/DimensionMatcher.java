d35aa1b208792820ae76dd4c98fd74d9f5edb63f
Daniel Hazelton <dshadowwolf@gmail.com>
2021-07-05 16:59:32 -0400

    Some work on fleshing out biome and dimension allow/deny lists
    Final resolution of spawns - which need occur, necessarily, at the end
    of the block registry event (the start of the item registry event, at a
    minimum) is the next step. Will need to create a BlockMatcher setup to
    take over for the Replacements and a generic utility for turning
    OS4BlockData into a BlockState.

--- src/main/java/com/mcmoddev/orespawn/builders/DimensionMatcher.java
+++ src/main/java/com/mcmoddev/orespawn/builders/DimensionMatcher.java
@@ -1,11 +1,43 @@
 package com.mcmoddev.orespawn.builders;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.google.gson.JsonArray;
 
-public class DimensionMatcher {
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Dimension;
 
-	public DimensionMatcher(JsonArray jsonArray) {
-		// TODO Auto-generated constructor stub
+public class DimensionMatcher {
+	private final List<RegistryKey<Dimension>> allowedDimensions;
+	private final List<String> rawDimensions;
+	private boolean loaded = false;
+	
+	public DimensionMatcher(JsonArray dimensionData) {
+		allowedDimensions = new LinkedList<>();
+		rawDimensions = new LinkedList<>();
+		dimensionData.forEach(dim -> rawDimensions.add(dim.getAsString()));	
 	}
 
+	private RegistryKey<Dimension> getKeyFromString(final String dimensionName) {
+		String mod = dimensionName.contains(":")?dimensionName.split(":")[0]:"minecraft";
+		String name = dimensionName.contains(":")?dimensionName.split(":")[1]:dimensionName;
+		ResourceLocation loc = new ResourceLocation(mod, name);
+		return RegistryKey.getOrCreateKey(Registry.DIMENSION_KEY, loc);
+	}
+	public boolean isDimensionAllowed(final String dimension) {
+		if (allowedDimensions.isEmpty() || !loaded) return true;
+		return allowedDimensions.contains(getKeyFromString(dimension));
+	}
+	
+	public void resolveData() {
+		allowedDimensions.addAll(
+				rawDimensions.stream()
+				.map( rawDim -> getKeyFromString(rawDim) )
+				.collect(Collectors.toList()));
+		loaded = true;
+	}
 }
